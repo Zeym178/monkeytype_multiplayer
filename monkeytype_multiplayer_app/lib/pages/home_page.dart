1b242ede3600702dd7c3337d56f8cfe59b8dc931
Zeym <zeemancoro@gmail.com>
2025-03-14 17:57:48 -0500

    update

--- monkeytype_multiplayer_app/lib/pages/home_page.dart
+++ monkeytype_multiplayer_app/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FocusNode _focusNode = FocusNode();
+  final String TypeText = "hola como estamos el dia de hoy";
+  late String userText;
+  int originalIndex = 0, userIndex = 0;
+  late int originalSize;
+  List<Map> _text = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    originalSize = TypeText.length;
+    userText = TypeText;
+    for (int i = 0; i < TypeText.length; i++) {
+      _text.add({"char": TypeText[i], "color": 1});
+    }
+    super.initState();
+  }
+
+  void compare(String key) {
+    setState(() {
+      if (key == 'Backspace') {
+        if (userIndex > originalIndex) {
+          userText =
+              userText.substring(0, userIndex - 1) +
+              TypeText.substring(originalIndex);
+          userIndex--;
+        }
+        return;
+      }
+      if (key != TypeText[originalIndex]) {
+        userText =
+            userText.substring(0, userIndex) +
+            key +
+            TypeText.substring(originalIndex);
+        userIndex++;
+        return;
+      }
+      userIndex++;
+      originalIndex++;
+    });
+    print(TypeText);
+  }
+
   @override
   Widget build(BuildContext context) {
     final mytheme = Theme.of(context).colorScheme;
@@ -76,27 +120,45 @@ class _HomePageState extends State<HomePage> {
                 ],
               ), // up bar
             ),
-            Container(
-              margin: EdgeInsets.all(50),
-              child: Stack(
-                children: [
-                  Center(
-                    child: Container(
-                      color: Colors.red,
-                      child: TextField(
-                        style: TextStyle(fontSize: 20),
-                        decoration: InputDecoration(hintText: 'I suck :('),
+            Expanded(
+              child: GridView.builder(
+                itemCount: _text.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 50,
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: double.infinity,
+                    child: Text(
+                      _text[index]['char'],
+                      style: TextStyle(
+                        color:
+                            _text[index]['color'] == 1
+                                ? Colors.green
+                                : Colors.red,
                       ),
                     ),
-                  ),
-                  Center(
-                    child: Container(
-                      color: Colors.grey.withOpacity(.3),
-                      child: TextField(style: TextStyle(fontSize: 20)),
-                    ),
-                  ),
-                ],
+                  );
+                },
               ),
+            ),
+            KeyboardListener(
+              focusNode: _focusNode,
+              autofocus: true,
+              onKeyEvent: (KeyEvent event) {
+                final label = event.logicalKey.keyLabel;
+                final keychar = event.character;
+                if (event is KeyRepeatEvent || event is KeyDownEvent) {
+                  if (keychar != null) {
+                    print(keychar);
+                    compare(keychar);
+                  } else {
+                    print(label);
+                    compare(label);
+                  }
+                }
+              },
+              child: Container(),
             ),
             Container(height: 30, color: Colors.blue),
           ],
